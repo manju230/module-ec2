7d942fd3220211453fe3204ccaaaac57e8a315b0
manju230 <111728745+manju230@users.noreply.github.com>
2023-04-18 12:32:04 +0530

    acccount

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ module "ec2_instance" {
   ami                    = "ami-09461328af8fbcb9c"
   instance_type          = "t2.micro"
   key_name               = "Golden Key"  
-  vpc_security_group_ids = ["sg-039346231eed4115b"]
+  vpc_security_group_ids = ["sg"]
   subnet_id              = "subnet-01f99bd1078deccd8"
   disable_api_termination = false
   associate_public_ip_address = false
-  iam_instance_profile = "arn:aws:iam::034146468059:instance-profile/SSM-ROLE"
+  iam_instance_profile = "arn:aws:iam::ACCOUNT-NUMBER:instance-profile/SSM-ROLE"
 
   tags = {
     Terraform   = "true"
